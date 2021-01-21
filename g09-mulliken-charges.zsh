27f6caffe77e851ddf83c529f820d6cd85711785
conormcgee-cyrpto <72422507+conormcgee-cyrpto@users.noreply.github.com>
2021-01-21 11:36:16 -0500

    Add files via upload

---
+++ g09-mulliken-charges.zsh
@@ -0,0 +1,79 @@
+#!/usr/bin/env zsh
+set  -f
+#
+# Gaussian09 Mulliken charge for specific atom
+#
+
+function g09-mulliken-charges()
+{
+	if [[ $(${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-revision.zsh "${1}") == "Gaussian 09, Revision D.01" ]]
+	then # using Gaussian 09, Revision D.01
+
+		if [[ -z "${2}" ]]
+		then # all Mulliken charges are requested
+
+			data=$(
+				awk '/Mulliken charges and spin densities:/,/Sum of Mulliken charges/ { print }' "${1}" |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					sed -n '1,/               1          2/p' |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					awk '{print $2, $3}' |\
+					column -t
+			)
+
+		else # Mulliken charges of a specific atom are requested
+
+			data=$(
+				awk '/Mulliken charges and spin densities:/,/Sum of Mulliken charges/ { print }' "${1}" |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					sed -n '1,/               1          2/p' |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					awk -v atom="${2}" '$0 ~ atom {print $3}'
+			)
+
+		fi
+
+	elif [[ $(${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-revision.zsh "${1}") == "Gaussian 09, Revision C.01" ]]
+	then # using Gaussian 09, Revision C.01
+
+		if [[ -z "${2}" ]]
+		then # all Mulliken charges are requested
+
+			data=$(
+				awk '/Mulliken atomic charges:/,/Sum of Mulliken atomic charges/ { print }' "${1}" |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					sed -n '1,/             1/p' |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					awk '{print $2, $3}' |\
+					column -t
+			)
+
+		else # Mulliken charges of a specific atom are requested
+
+			data=$(
+				awk '/Mulliken atomic charges:/,/Sum of Mulliken atomic charges/ { print }' "${1}" |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					sed -n '1,/             1/p' |\
+					head -n -1 |\
+					/usr/bin/tail -r |\
+					awk -v atom="${2}" '$0 ~ atom {print $3}'
+			)
+		fi
+	else # perhaps unsupported?
+		data="Gaussian revision unsupported"
+	fi
+
+	"${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-data-test.zsh" "$data"
+}
+
+# g09-mulliken-charges file [atom]
+g09-mulliken-charges "${1}" "${2}"
+
+
---
+++ g09-number-of-basis-functions.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+set  -f
+#
+#
+# number of basis functions
+#
+
+function g09-number-of-basis-functions()
+{
+	local data="$(awk '/basis functions,/ { result = $1 } END {print result}' "${1}")"
+
+	"${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-data-test.zsh" "$data"
+}
+
+g09-number-of-basis-functions "${1}"
+
+
